316c39684047bc1649090c076d75af3585bcc398
ryanzhxu <ryanzhxu@students.cs.ubc.ca>
2021-12-03 21:59:26 -0800

    Updated bootstrap files.

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -13,7 +13,8 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
-            return View();
+
+            return View(movie);
         }
     }
 }
