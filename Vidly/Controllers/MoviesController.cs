dbbeb0e303864be322c0d018b23d0da901f4311f
ryanzhxu <ryanzhxu@students.cs.ubc.ca>
2021-12-05 00:53:25 -0800

    Fixed a bug where user cannot edit a movie.

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -34,10 +34,14 @@ namespace Vidly.Controllers
             return View("MovieForm", vm);
         }
 
-        public ActionResult Create(Movie movie)
+        [HttpPost]
+        public ActionResult Save(Movie movie)
         {
             if (movie.Id == 0)
+            {
+                movie.DateAdded = DateTime.Now;
                 _context.Movies.Add(movie);
+            }  
             else
             {
                 var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -14,14 +14,15 @@ namespace Vidly.Models
         [StringLength(255)]
         public string Name { get; set; }
 
-        [Required]
         public Genre Genre { get; set; }
 
         [Display(Name = "Genre")]
+        [Required]
         public byte GenreId { get; set; }
 
         public DateTime DateAdded { get; set; }
 
+        [Display(Name = "Release date")]
         public DateTime ReleaseDate { get; set; }
 
         [Display(Name = "Number in stock")]
