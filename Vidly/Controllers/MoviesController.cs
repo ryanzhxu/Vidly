8182a718aae95811861d4533bf68ebf4e4191314
ryanzhxu <ryanzhxu@students.cs.ubc.ca>
2021-12-04 20:19:53 -0800

    Added migrations to populate Genres table

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -26,14 +27,14 @@ namespace Vidly.Controllers
         public ViewResult Index()
         {
             //var movies = GetMovies();
-            var movies = _context.Movies.ToList();
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
 
             return View(movies);
         }
 
         public ActionResult Details(int id)
         {
-            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
 
             if (movie == null)
                 return HttpNotFound();
