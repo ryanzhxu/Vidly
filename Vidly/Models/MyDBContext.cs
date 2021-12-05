930e9fc8e4df2bae0d3fe1970b8c99939ce523aa
ryanzhxu <ryanzhxu@students.cs.ubc.ca>
2021-12-04 18:25:42 -0800

    Added first migration

---
+++ Vidly/Models/MyDBContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Vidly.Models
+{
+    public class MyDBContext:DbContext
+    {
+        public MyDBContext()
+        {
+
+        }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
